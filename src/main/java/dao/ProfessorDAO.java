36b2984876983e88fa702c86a8dfe30e0ab2c537
Alexsandro Paulino Dantas <alexsandropdantas@gmail.com>
2022-08-30 22:15:10 -0300

    Adicionando Classes Professor

---
+++ src/main/java/dao/ProfessorDAO.java
@@ -0,0 +1,17 @@
+package dao;
+
+import java.util.ArrayList;
+
+import model.Professor;
+
+
+public interface ProfessorDAO {
+
+	void criarProfessor(Professor professor);       // C - Create
+    ArrayList<Professor> getTodosProfessores();     // R - Read
+    Professor getProfessorPorCpf(String cpf);       // R - Read
+    void atualizarProfessor(Professor professor);   // U - Update (atualizar)
+    void deleteProfessor(Professor professor);      // D - Delete
+    void deleteProfessor(String cpf);               // D - Delete
+
+}
